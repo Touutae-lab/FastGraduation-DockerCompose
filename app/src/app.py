656ef442e59a7de237ad105e641e5e6b899670d5
Saksinkarn Petchkuljinda <dward.the.2nd@gmail.com>
2022-07-15 20:08:49 +0700

    Fix bug related to Flask's app module

--- app/src/app.py
+++ app/src/app.py
@@ -1,23 +1,25 @@
 import redis
 from flask import Flask
 
-import core
+import os
+from core import get_hit_count
 
-app: Flask = Flask(__name__)
-cache: redis.Redis = redis.Redis(host="redis", port=6379)
+app = Flask(__name__)
+cache = redis.Redis(host="redis", port=6379)
 
 
 @app.route("/")
-def hello() -> str:
-    count: int = core.get_hit_count(cache)
+def hello():
+    count: int = get_hit_count(cache)
     return "Hello World! I have been seen {} times.\n".format(count)
 
 
 @app.route("/getall")
-def getAll() -> str:
+def getAll():
     return "kuy"
 
 
 @app.route("/hee")
-def hee() -> str:
+def hee():
     return "CMU SO FUN"
+
